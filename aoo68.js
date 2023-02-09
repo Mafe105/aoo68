4b5453053f68632118edd07c7c3624d4c1ea8764
Estudiante <estuam@UAM.LOCAL>
2023-02-09 12:01:48 -0500

    plantilla de menu

---
+++ aoo68.js
@@ -0,0 +1,7 @@
+let valor1 = document.getElementById("v1");
+
+let span = document.getElementsByTagName("span")[0];
+span.innerHTML = "saludos";
+
+let resultado=document.getElementsByClassName("resultado1")[0];
+resultado.innerHTML = "el resultado es:";
